5991ef21b983847111f299836708b3805443a3ed
bvddw <galaid.arseniy@gmail.com>
2023-09-06 15:10:35 +0300

    task 5 modified

--- lesson1/task5.py
+++ lesson1/task5.py
@@ -3,10 +3,10 @@ import cmath
 
 # Task 5
 print('task 5:')
-# xr = float(input('\tEnter real part of x: '))
-# xi = float(input('\tEnter img part of x: '))
-# x = complex(xr, xi)
-x = complex(input())
+xr = float(input('\tEnter real part of x: '))
+xi = float(input('\tEnter img part of x: '))
+x = complex(xr, xi)
+# x = complex(input())
 print(f'\tYour complex number: {x}')
 y = (math.cos(math.log(7)) * (cmath.sin(7 * x) ** 2)) / (7 * cmath.cos(14 * x))
 yr = complex(round(y.real, 4), round(y.imag, 4))
