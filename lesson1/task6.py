65d091a52911c719ba91a01ed85cc4af95175b60
galaida <galaida.arseniy@gmail.com>
2023-09-18 17:23:59 +0300

    small fixes

--- lesson1/task6.py
+++ lesson1/task6.py
@@ -5,11 +5,11 @@ from turtle import *
 reset()
 
 kx = 50
-ky = 500
+ky = 50
 
 color('red')
 width(2)
-maxy = 0.5
+maxy = 5
 up()
 goto(-kx * 2 * math.pi, 0)
 down()
@@ -22,14 +22,14 @@ goto(0, - ky * maxy)
 color('blue')
 width(3)
 
-x = math.pi / 25
+x = math.pi / 250
 y = 2 * math.log(x) + x ** 2 - 4 * x + 3
-dx = math.pi / 25
+dx = math.pi / 250
 up()
 goto(kx * x, ky * y)
 down()
 
-for i in range(100):
+for i in range(350):
     y = 2 * math.log(x) + x ** 2 - 4 * x + 3
     goto(kx * x, ky * y)
     x += dx
--- lesson2/task5.py
+++ lesson2/task5.py
@@ -5,5 +5,10 @@ right = 100
 numbers = [randint(left, right) for i in range(20)]
 print(numbers)
 a = int(input("Enter number: "))
-suma = sum(filter(lambda item: item < a, numbers))
-print(f"Suma of items, that less than a ({a}): {suma}")
+suma = 0
+count = 0
+for item in numbers:
+    if item < a:
+        suma += item
+        count += 1
+print(f"Suma of items, that less than a ({a}): {suma}, amount: {count}")
